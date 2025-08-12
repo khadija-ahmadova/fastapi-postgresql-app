11277d15542ba98c9aedf958a2e8a3a53f1e7657
khadija-ahmadova <xadija.ahmedova@gmail.com>
2025-08-12 15:26:56 +0400

    create tables and connect db

---
+++ database.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+load_dotenv
+database_url = os.getenv("DATABASE_URL")
+
+engine = create_engine(database_url)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
